518f02a8499b669076883fe696327ef56208b7ff
bmael <maelbar44@gmail.com>
2013-04-23 13:37:30 +0200

    Adding Db - Correcting some resharper warning

--- src/Tests/DBManagerTest.cs
+++ src/Tests/DBManagerTest.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
 {
-    using System.Diagnostics.CodeAnalysis;
-
     /// <summary>
     /// The database manager tests.
     /// </summary>
