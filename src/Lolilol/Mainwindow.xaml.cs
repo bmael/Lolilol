8cde310cf2e894a7707c9a95c932c25eca48026e
Niiner <florian.fagniez@etu.univ-nantes.fr>
2013-04-23 13:59:31 +0200

    Resharper modification code : methods not implemented yet

--- src/Lolilol/Mainwindow.xaml.cs
+++ src/Lolilol/Mainwindow.xaml.cs
@@ -1,30 +1,166 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Mainwindow.xaml.cs" company="Kiss Fairy Corporation">
+//   
+// </copyright>
+// <summary>
+//   Logique d'interaction pour Mainwindow.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
-namespace Lolilol.View
+namespace Lolilol
 {
+    using System;
+    using System.Windows;
+
     /// <summary>
     /// Logique d'interaction pour Mainwindow.xaml
     /// </summary>
     public partial class Mainwindow : Window
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Mainwindow"/> class.
+        /// </summary>
         public Mainwindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// The window_ loaded.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // Not implemented Yet
+        }
+
+        /// <summary>
+        /// Allow a user to close Lolilol Application Memento.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        private void ClickClose(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Allow a user to display all champions of the game.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        private void ClickAllChamp(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        /// <summary>
+        /// Allow a user to display all champions of the week.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void ClickCow(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Allow a user to display all items of the game.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        void ClickAllItems(object sender, RoutedEventArgs e)
+        {
+    
+        }
+
+        /// <summary>
+        /// Allow a user to display all builds of the game.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        private void ClickAllBuilds(object sender, RoutedEventArgs e)
+        {
+       
+        }
+
+        /// <summary>
+        /// Allow a user to create his own build for his character.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        private void ClickYob(object sender, RoutedEventArgs e)
+        {
+    
+        }
+
+        /// <summary>
+        /// Allow a user to create his own timer for the game.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        private void ClickYot(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Allow a user to know more about us.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        private void ClickAboutUs(object sender, RoutedEventArgs e)
         {
 
         }
