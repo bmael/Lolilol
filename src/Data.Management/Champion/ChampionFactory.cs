84ba4934cc158543e30176a28ef7152b4eae3dd5
Mael Barbin <m.barbin@dental-moving.com>
2013-05-10 23:18:45 +0200

    sqlite problem solved

---
+++ src/Data.Management/Champion/ChampionFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.Management.Champion
+{
+    using Common.Champion;
+    using Common.Data;
+
+    /// <summary>
+    /// The champion factory.
+    /// </summary>
+    public class ChampionFactory : FactoryBase<IChampion, Champion>
+    {
+    }
+}
